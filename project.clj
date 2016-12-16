864b28e6f7394550a5ab4802c04896b046027d61
Tim Pote <tim.pote@cicayda.com>
2016-12-16 13:57:03 -0600

    Uptick version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-reloaded/lein-template "0.1.1"
+(defproject cljs-reloaded/lein-template "0.2.0"
   :description "Easily start a new cljs project with reloading."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
