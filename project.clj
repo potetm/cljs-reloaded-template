3cde4e682fdfc7e5588f3751880f2289334dc757
Tim Pote <tim.pote@cicayda.com>
2016-12-15 18:55:23 -0600

    No snapshot.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-reloaded/lein-template "0.1.0-SNAPSHOT"
+(defproject cljs-reloaded/lein-template "0.1.1"
   :description "Easily start a new cljs project with reloading."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
